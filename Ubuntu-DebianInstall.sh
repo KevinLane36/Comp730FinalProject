5c2f98a3be5934497891e0d2c1f1e483d43a3d7b
KevinLane36 <32558498+KevinLane36@users.noreply.github.com>
2021-04-26 15:31:37 -0400

    Create Ubuntu-DebianInstall.sh

---
+++ Ubuntu-DebianInstall.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Installing PSQL and PGBench
+sudo apt install postgresql postgresql-contrib -y
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+
+echo "Installing sysbench"
+curl -s https://packagecloud.io/install/repositories/akopytov/sysbench/script.deb.sh | sudo bash
+sudo apt -y install sysbench
+
+echo "Installing fio"
+sudo apt -y install fio
