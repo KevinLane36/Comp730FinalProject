8c58afb374ba336d9f315cca5328fcfdd010a83d
Ibrahim Mubarek <imubarek@Ibrahims-MacBook-Air.local>
2021-04-26 15:48:36 -0400

    Added FIO script

---
+++ FIOFiles/FIOJobFiles/README.md
@@ -0,0 +1,9 @@
+The following are descriptions for each of the job files:
+
+randomReadJob.fio: Randomly reads a total of 2GB. Runs 4 processes with each randomly reading 512MB.
+
+randomWriteJob.fio: Randomly writes a total of 2GB. Runs 4 processes with each randomly writing 512MB.
+
+sequentialReadJob.fio: Sequentially reads a total of 2GB. Runs 4 processes with each sequentially reading 512MB.	
+
+sequentialWriteJob.fio: Sequentially writes a total of 2GB. Runs 4 processes with each sequentially writing 512MB.
---
+++ FIOFiles/fioScript.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+
+hypervisor = $1
+host = $2
+guest = $3
+ram = $4
+
+
+echo "Starting FIO File Tests"
+echo "Starting new FIO Tests" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting random read test" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+fio randomReadJob.fio >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting random write test" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt 
+fio randomWriteJob.fio >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting sequential read test" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+fio sequentialReadJob.fio >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting sequential write test" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+fio sequentialWriteJob.fio >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Finished running FIO Tests" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Finished running FIO File Tests"
+
