ab11dc49f45ff16c92fe472fd1b8661379eb0646
KevinLane36 <32558498+KevinLane36@users.noreply.github.com>
2021-04-26 16:17:10 -0400

    Update fioScript.sh

--- FIOFiles/fioScript.sh
+++ FIOFiles/fioScript.sh
@@ -8,15 +8,21 @@ ram=$4
 
 
 echo "Starting FIO File Tests"
+echo "Starting new FIO Tests"
 echo "Starting new FIO Tests" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting random read test"
 echo "Starting random read test" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
 fio FIOJobFiles/randomReadJob.fio >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting random write test" 
 echo "Starting random write test" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt 
 fio FIOJobFiles/randomWriteJob.fio >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting sequential read test" 
 echo "Starting sequential read test" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
 fio FIOJobFiles/sequentialReadJob.fio >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting sequential write test"
 echo "Starting sequential write test" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
 fio FIOJobFiles/sequentialWriteJob.fio >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Finished running FIO Tests"
 echo "Finished running FIO Tests" >> fioResults_${hypervisor}_${host}_${guest}_${ram}.txt
 echo "Finished running FIO File Tests"
 
