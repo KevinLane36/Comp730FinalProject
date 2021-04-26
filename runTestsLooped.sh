2bf7607ec7b529f34fa74fada3e8bb2cb96c308a
KevinLane36 <32558498+KevinLane36@users.noreply.github.com>
2021-04-26 16:30:46 -0400

    Update runTestsLooped.sh

--- runTestsLooped.sh
+++ runTestsLooped.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 
+if [ "$#" -ne 4 ]; then
+    echo "Illegal number of parameters. Please provider [hypervisor host guest ram]"
+fi
+
+hypervisor=$1
+host=$2
+guest=$3
+ram=$4
+
+
 for i in 1 2 3 4 5
 do
-   bash FIOFiles/fioScript.sh
-   bash pgbench-sysbenchScript.sh
+   bash FIOFiles/fioScript.sh hypervisor host guest ram
+   bash pgbench-sysbenchScript.sh hypervisor host guest ram
 done
