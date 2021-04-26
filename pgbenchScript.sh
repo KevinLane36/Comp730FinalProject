0bf918127c846cb31a063be56af01a149a800848
KevinLane36 <32558498+KevinLane36@users.noreply.github.com>
2021-04-26 15:25:24 -0400

    Update pgbenchScript.sh

--- pgbenchScript.sh
+++ pgbenchScript.sh
@@ -16,7 +16,7 @@ pgbench -c 10 -j 1 -t 100 testdb >> pgbenchResult_${hypervisor}_${host}_${guest}
 
 echo "Starting sysbench Test"
 echo "Starting new sysbench Test" >> sysbenchResults_${hypervisor}_${host}_${guest}_${ram}.txt
-echo "Starting CPU test" >> sysbenchResults.txt
+echo "Starting CPU test" >> sysbenchResults_${hypervisor}_${host}_${guest}_${ram}.txt
 sysbench --test=cpu --cpu-max-prime=20000 run >> sysbenchResults_${hypervisor}_${host}_${guest}_${ram}.txt
 echo "Starting Mem test" >> sysbenchResults.txt
 sysbench --test=memory --memory-block-size=1M --memory-total-size=4G --num-threads=1 run >> sysbenchResults_${hypervisor}_${host}_${guest}_${ram}.txt
