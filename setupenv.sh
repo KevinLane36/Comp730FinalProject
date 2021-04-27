5fada9350f4f25b5f29dcff254ff4a11bbb50b80
Justin Do <34545607+justinleedo@users.noreply.github.com>
2021-04-27 17:59:55 -0400

    Update setupenv.sh

--- setupenv.sh
+++ setupenv.sh
@@ -3,5 +3,6 @@ cp fioScript.sh ../fioScript.sh
 cp pgbench-sysbenchScript.sh ../pgbench-sysbenchScript.sh
 cp network.sh ../network.sh
 cp runTestsLooped.sh ../runTestsLooped.sh
+cp networkLoop.sh ../networkLoop.sh
 cp Ubuntu-DebianInstall.sh ../Ubuntu-DebianInstall.sh
 cp CentosInstall.sh ../CentosInstall.sh
