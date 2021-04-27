b76c30c011ebab82ae8ba1abfbd384d8cf76ea5b
KevinLane36 <32558498+KevinLane36@users.noreply.github.com>
2021-04-27 15:01:20 -0400

    Update network.sh

--- network.sh
+++ network.sh
@@ -6,15 +6,22 @@ guest=$3
 ram=$4
 ipaddr=$5
 
-tcp_time="tcp_time_${hypervisor}_${host}_${guest}_${ram}.json"
-udp_time="udp_time_${hypervisor}_${host}_${guest}_${ram}.json"
-tcp_space="tcp_space_${hypervisor}_${host}_${guest}_${ram}.json"
-udp_space="udp_space_${hypervisor}_${host}_${guest}_${ram}.json"
 
-iperf3 -c $ipaddr -p5201 -t10 b-1000000000 -J --logfile $tcp_time
-iperf3 -c $ipaddr -p5201 -t10 b-1000000000 -J -u --logfile $udp_time
-iperf3 -c $ipaddr -p5201 -k1000 -J --logfile $tcp_space
-iperf3 -c $ipaddr -p5201 -k1000 -J -u --logfile $udp_space
+
+echo "Starting PGBench Test"
+echo "Starting new iperf Test" >> iperfResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting tcp_time test"
+echo "Starting tcp_time test" >> iperfResults_${hypervisor}_${host}_${guest}_${ram}.txt
+iperf3 -c $ipaddr -p5201 -t10 b-1000000000 -J >> iperfResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting udp_time test"
+echo "Starting udp_time test" >> iperfResults_${hypervisor}_${host}_${guest}_${ram}.txt
+iperf3 -c $ipaddr -p5201 -t10 b-1000000000 -J -u >> iperfResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting tcp_space test"
+echo "Starting tcp_space test" >> iperfResults_${hypervisor}_${host}_${guest}_${ram}.txt
+iperf3 -c $ipaddr -p5201 -k1000 -J >> iperfResults_${hypervisor}_${host}_${guest}_${ram}.txt
+echo "Starting udp_space test"
+echo "Starting udp_space test" >> iperfResults_${hypervisor}_${host}_${guest}_${ram}.txt
+iperf3 -c $ipaddr -p5201 -k1000 -J -u >> iperfResults_${hypervisor}_${host}_${guest}_${ram}.txt
 
 
 echo "Running $guest on $host using $hypervisor with $ram GB of RAM."
